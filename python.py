9b2bceff608b31d6cff37009cacd58ba43d8d564
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2024-12-31 05:15:16 +0530

    py

--- python.py
+++ python.py
@@ -1,2 +1,15 @@
 var_1 = 'Dilip kumar';
-print(var_1);
+# print(var_1);
+
+
+name,age,address,ph_no = 'Dilip kumar',23,'chennai',9176564700;
+
+print(name);
+print(ph_no);
+
+# type casting : 
+mark = 78;
+print("my mark is " + str(mark));
+print(type(mark));
+
+# user_input : 

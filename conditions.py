faa1eda2efcada987165e5e555384b6aea5d4268
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2024-12-31 08:10:48 +0530

    py

--- conditions.py
+++ conditions.py
@@ -10,11 +10,49 @@
 
 
 #1:
-in_num = int(input('say one number : '));
+# in_num = int(input('say one number : '));
 
-if in_num > 0 :
-    print('positive number');
-elif in_num < 0 :
-    print('negative number');
+# if in_num > 0 :
+#     print('positive number');
+# elif in_num < 0 :
+#     print('negative number');
+# else:
+#     print('zero');
+
+
+#2:
+# in_num = int(input('say one number : '));
+
+# if in_num > 0 :
+#     print('positive number');
+#     if in_num % 2 == 0:
+#         print('even number');
+#     elif in_num % 2 != 0:
+#         print('odd number');
+#     else:
+#         print('invalid input');
+
+# elif in_num < 0 :
+#     print('negative numbers are not acceptable');
+# else:
+#     print('zero');
+
+
+#3:
+mrk = int(input('say marks : '));
+
+if mrk <= 100 and mrk >= 0 :
+    if mrk >= 90 :
+        print('A - grade');
+    elif mrk >= 80 :
+        print('B - grade');
+    elif mrk >= 70 :
+        print('C - grade');
+    elif mrk >= 50 :
+        print('D - grade');
+    elif mrk < 50 and mrk >= 35 :
+        print('just pass E - grade');
+    else:
+        print('fail de-grade');
 else:
-    print('zero');
+    print('Invalid input');

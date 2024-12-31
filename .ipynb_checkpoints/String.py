710d756f5a39e561264976229ab49ed0f8d21dd3
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2024-12-31 20:56:39 +0530

    py

--- .ipynb_checkpoints/String.py
+++
@@ -1,7 +0,0 @@
-#slicing using indexing : 
-
-name = 'Dilip kumar';
-
-cut_mark = index(' ');
-
-print(name[:cut_mark]);
--- temo.py
+++ temo.py
@@ -1,1 +1,10 @@
-#logical operators: 
+#Strings : 
+
+
+#slicing using indexing : 
+
+name = 'Dilip kumar';
+
+cut_mark = name.find(' ');
+
+print(name[:cut_mark]);

a55a9ff146eda0fb97f864c7b3830c9d09ba421d
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2024-12-31 11:40:52 +0530

    py

--- .ipynb_checkpoints/conditions-checkpoint.py
+++ .ipynb_checkpoints/conditions-checkpoint.py
@@ -60,29 +60,29 @@
 
 #4:
 
-acc_holder = 'DK';
-suff_balance = 500;
-balance = 2000;
-hold_balance = balance - suff_balance;
-og_pin = '1234';
+# acc_holder = 'DK';
+# suff_balance = 500;
+# balance = 2000;
+# hold_balance = balance - suff_balance;
+# og_pin = '1234';
 
-pin = str(input('Enter your pin : '));
+# pin = str(input('Enter your pin : '));
 
-if pin == og_pin :
-    print('welcome to otaaku bank...🚀');
-    print('account holder : ' + acc_holder);
-    print('Bank_balance : ' + str(balance));
-    print('Sufficient balance : ' + str(suff_balance));
-    print('you get from this => Available balance : ' + str(hold_balance));
-    needed_amount = int(input('enter the amount you need to retrieve : '));
-    if needed_amount > 0 and needed_amount <= hold_balance:
-        hold_balance -= needed_amount;
-        print('Transaction is successfully completed ✅');
-        print('your new available balance is : ' + str(hold_balance) + '😊❤️🚀');
-        print('Bye Bye...')
-    elif needed_amount > 0 and needed_amount >= hold_balance :
-        print('Insufficient Balance');
-    else:
-        print('Enter amount above zero ');
-else:
-    print('incorrect pin');
+# if pin == og_pin :
+#     print('welcome to otaaku bank...🚀');
+#     print('account holder : ' + acc_holder);
+#     print('Bank_balance : ' + str(balance));
+#     print('Sufficient balance : ' + str(suff_balance));
+#     print('you get from this => Available balance : ' + str(hold_balance));
+#     needed_amount = int(input('enter the amount you need to retrieve : '));
+#     if needed_amount > 0 and needed_amount <= hold_balance:
+#         hold_balance -= needed_amount;
+#         print('Transaction is successfully completed ✅');
+#         print('your new available balance is : ' + str(hold_balance) + '😊❤️🚀');
+#         print('Bye Bye...')
+#     elif needed_amount > 0 and needed_amount >= hold_balance :
+#         print('Insufficient Balance');
+#     else:
+#         print('Enter amount above zero ');
+# else:
+#     print('incorrect pin');

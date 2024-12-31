b8f409c84aa9043f6d6d25a1f71768f8112f06bc
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2024-12-31 20:04:34 +0530

    py

--- .ipynb_checkpoints/cal.py
+++ .ipynb_checkpoints/cal.py
@@ -1,27 +1,28 @@
 #lets build low level calculator : 
 
-operator = str(input('Enter an operator(+ , - , * , /) : '));
-num_1 = int(input('Enter a num_1 : '));
-num_2 = int(input('Enter a num_2 : '));
-
-calculater(operator, num_1, num_2);
-
 def calculater(operator, num_1, num_2):
     if operator == '+' :
-        result = (num_1 + num_2);
+        result = round(num_1 + num_2);
         print(result);
         return result ; 
     elif operator == '-' :
-        result = (num_1 - num_2);
+        result = round(num_1 - num_2);
         print(result);
         return result ;
     elif operator == '*' :
-        result = (num_1 * num_2);
+        result = round(num_1 * num_2);
         print(result);
         return result ;
     elif operator == '/' :
-        result = (num_1 / num_2);
+        result = round(num_1 / num_2);
         print(result);
         return result ;
     else:
         print('Invaild input');
+
+
+operator = str(input('Enter an operator(+ , - , * , /) : '));
+num_1 = float(input('Enter a num_1 : '));
+num_2 = float(input('Enter a num_2 : '));
+
+calculater(operator, num_1, num_2);

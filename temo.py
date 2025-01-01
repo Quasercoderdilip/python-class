eb17245be53ba718171de296f96b084708818b34
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-01 10:13:20 +0530

    py

--- temo.py
+++ temo.py
@@ -3,8 +3,8 @@
 
 #slicing using indexing : 
 
-name = 'Dilip kumar';
+# name = 'Dilip kumar';
 
-cut_mark = name.find(' ');
+# cut_mark = name.find(' ');
 
-print(name[:cut_mark]);
+# print(name[:cut_mark]);

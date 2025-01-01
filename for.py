951e6bd389ba02bee3173c6ecef8e9b4bae23ef4
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-01 15:33:34 +0530

    py

--- for.py
+++ for.py
@@ -1,11 +1,29 @@
 #for loop:
 
-txt = 'Hello iam Robot';
-ind = [];
+# txt = 'Hello iam Robot';
+# ind = [];
 
-for i,char in enumerate(txt):
-    if(char == ' '):
-        ind.append(i);
-        continue;
+# for i,char in enumerate(txt):
+#     if(char == ' '):
+#         ind.append(i);
+#         continue;
 
-print(ind);
+# print(ind);
+
+
+#sum of all nums:
+# n = 10;
+# sumof = 0;
+
+# for i in range(1,n+1):
+#     sumof += i;
+
+# print(sumof);
+
+
+#vowels finder:
+
+txt = str(input('Enter your text : '));
+vowels = [a,e,i,o,u];
+
+

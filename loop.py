3df0b39b05a446f2f33dbbae96fc419d7cf8c9d8
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-01 12:00:34 +0530

    py

--- loop.py
+++ loop.py
@@ -3,9 +3,19 @@
 
 #while loop:
 
-password = ''
+# password = ''
 
-while password != 'python123':
-    password = str(input('Enter your password : '));
+# while password != 'python123':
+#     password = str(input('Enter your password : '));
 
-print("Access granted!")
+# print("Access granted!");
+
+
+#continue keyword: to skip the rest of the current iteration and move to the next iteration.
+
+i = 0
+while i < 6:
+  i += 1;
+  if i == 3:
+    continue;# jump directly to the next iteration.
+  print(i);

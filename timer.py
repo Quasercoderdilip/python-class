46fef515780095a9e9c153acd2f62c412d6605ca
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-01 18:50:29 +0530

    py

--- timer.py
+++ timer.py
@@ -5,11 +5,21 @@ import time;#time module.
 
 
 
-time_in = int(input('Enter time in seconds : '));
+# time_in = int(input('Enter time in seconds : '));
 
-for i in range(time_in + 1):
-    time_in -= 1;
-    print(i);
-    time.sleep(1);
+# for i in range(time_in + 1):
+#     print(i);
+#     time.sleep(1);
 
-print('count is over');
+# print('count is over');
+
+
+
+#Reversed countdown timer : 
+# time_in = int(input('Enter time in seconds : '));
+
+# for i in range(time_in, 0,-1):
+#     print(i);
+#     time.sleep(1);
+
+# print('Happy new year !!! 🎆✨🎇🎈');

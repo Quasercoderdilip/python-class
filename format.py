ea4c720a9ecd1ac0cfcead42a8c565b5d83b12ea
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-01 11:19:19 +0530

    py

--- format.py
+++ format.py
@@ -11,3 +11,10 @@
 # :  = insert a space before positive numbers
 # :, = comma separator
 # :% = percentage format
+
+
+price1 = 3.14159;
+price2 = -987.65;
+price3 = 12.34;
+
+print(f'{price3:.1f}');

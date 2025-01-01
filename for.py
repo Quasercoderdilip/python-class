81a25fc22f25c1a9922f0062c359d80b3040784f
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-01 16:00:08 +0530

    py

--- for.py
+++ for.py
@@ -23,7 +23,42 @@
 
 #vowels finder:
 
-txt = str(input('Enter your text : '));
-vowels = [a,e,i,o,u];
+# txt = str(input('Enter your text : '));
+# vowels = ['a', 'e', 'i', 'o', 'u'];
+# no_vowels = 0 ;
+
+# for i,char in enumerate(txt):
+#     if(char.lower() in vowels):
+#         print(f'Vowel found at index ({i}) : {char}');
+#         no_vowels +=1;
+#         continue;
+
+# print(f'No of vowels : {no_vowels}');
 
 
+#Multiplication tables : 
+
+
+# table_num = int(input('Enter which table do you want :' ));
+# n = int(input('And Enter how many times do you want :' ));
+
+# for i in range(1,n + 1) : 
+#     print(f'{table_num} x {i} = {i * table_num}');
+
+
+#reverse the list:
+
+# n = ['a', 'e', 'i', 'o', 'u'];
+
+# for i in n[::-1] :
+#     print(i);
+
+#reversing name : 
+
+name = str(input('Enter your name : ')) ;
+rev_name = '';
+
+for char in name[::-1] :
+    rev_name += char;
+
+print(f'Reversed name : {rev_name}');

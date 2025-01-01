7035fc1ac6430b328ff57c49d77c72dcc5902f11
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-01 18:44:06 +0530

    py

--- timer.py
+++ timer.py
@@ -2,6 +2,14 @@
 
 import time;#time module.
 
-time.sleep(5);
 
-print('sleep is over');
+
+
+time_in = int(input('Enter time in seconds : '));
+
+for i in range(time_in + 1):
+    time_in -= 1;
+    print(i);
+    time.sleep(1);
+
+print('count is over');

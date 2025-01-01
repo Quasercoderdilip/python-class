21236707c53ab53933765e5278241cd1c73de2f7
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-01 15:08:04 +0530

    py

--- cal2.py
+++ cal2.py
@@ -7,21 +7,26 @@ time = 0;
 while True :
     principle = float(input('Enter your principle amount : '));
     if principle < 0 :
-        print('Priciple amount cannot be negative.');
+        print('Priciple amount cannot be negative or zero.');
     else :
         break;
 
 while True :
     rate = float(input('Enter your rate of intrest : '))
     if rate < 0 :
-        print('Rate of intrest cannot be negative.');
+        print('Rate of intrest cannot be negative or zero.');
     else :
         break;
 
 while True :
     time = float(input('Enter your time : '))
     if time < 0 :
-        print('Time of intrest cannot be negative.');
+        print('Time of intrest cannot be negative or zero.');
     else :
         break;
 
+
+total = principle * (1 + rate/100) ** time ; 
+
+print(f'Total amount {total}');
+print(f'Total amount of compound_Intrest is : {total - principle}');

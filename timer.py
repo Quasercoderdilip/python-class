315c5b192cb3045debdf547b69e428642fb21e5e
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-01 19:05:15 +0530

    py

--- timer.py
+++ timer.py
@@ -27,11 +27,11 @@ import time;#time module.
 
 #Timer : 
 
-time_in = int(input('Enter the max time in seconds :' ));
+# time_in = int(input('Enter the max time in seconds :' ));
 
-for i in range(time_in, 0, -1) :
-    seconds = i % 60;
-    minutes = int(i/60) % 60;
-    hours = int(i/3600);
-    time.sleep(1);
-    print(f'{hours:02} : {minutes:02} : {seconds:02}');
+# for i in range(time_in, 0, -1) :
+#     seconds = i % 60;
+#     minutes = int(i/60) % 60;
+#     hours = int(i/3600);
+#     time.sleep(1);
+#     print(f'{hours:02} : {minutes:02} : {seconds:02}');

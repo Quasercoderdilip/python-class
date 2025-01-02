a18483766983dd289411a06a9af6c483ce221ba4
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-02 19:56:13 +0530

    py

---
+++ meth.py
@@ -0,0 +1,9 @@
+#class methods : 
+
+#Class methods are methods that are bound to the class rather than an 
+#  of the class. They can be used to access and modify class-level data 
+# (such as class variables). Class methods are marked with the 
+# @classmethod decorator and take the class itself as their first parameter, 
+# conventionally named cls.
+
+

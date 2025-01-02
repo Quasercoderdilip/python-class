dc9482e07269011fae04c9293b49eabc119ca9f5
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-02 16:58:22 +0530

    py

--- c_2.py
+++ c_2.py
@@ -36,13 +36,70 @@
 # std_1.display();
 
 
-class Fruit():
-    def __init__(self):
-        self.color = '';
-    def display(self):
-        print(self.color);
+# class Fruit():
+#     def __init__(self,color):
+#         self.color = color;
+#     def display(self):
+#         print(self.color);
 
-apple = Fruit();
+# apple = Fruit('red');
 
 
-apple.display();
+# apple.display();
+
+
+# class employee:
+#     def __init__(self, name, age, salary):
+#         self.name = name;
+#         self.age = age;
+#         self.salary = salary;
+
+#     def display(self):
+#         print(f'his name is {self.name} and age is {self.age}, his salary is {self.salary}');
+
+
+
+# emp = employee('dilip',23,35500);
+# emp.display();
+
+
+# class teacher:
+#     def __init__(self,no,name,reg):
+#         self.no = no;
+#         self.name = name;
+#         self.reg_no = reg;
+#     def display(self):
+#         print(f'Name of teacher - {self.no} = {self.name}.');
+#         print(f'Register_no of teacher - {self.no} = {self.reg_no}.');
+
+
+# t_1 = teacher(1,'Meera','120766');
+# t_1.display();
+
+# t_2 = teacher(2,'pooja','120767');
+# t_2.display();
+
+
+class calci:
+    def __init__(self,a,b):
+        self.a = a;
+        self.b = b;
+
+    def add_display(self):
+        print(f'answer of add : {int(self.a + self.b)}');
+
+    def sub_display(self):
+        print(f'answer of sub : {int(self.a - self.b)}');
+
+    def mul_display(self):
+        print(f'answer of multiply : {int(self.a * self.b)}');
+
+    def div_display(self):
+        print(f'answer of division : {int(self.a / self.b)}');
+
+
+num_1 = calci(15,3);
+num_1.add_display();
+num_1.sub_display();
+num_1.mul_display();
+num_1.div_display();

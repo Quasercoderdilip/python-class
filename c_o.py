a79facfed4b77eda92c8da106e34a221e8524b01
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-02 14:54:18 +0530

    py

--- c_o.py
+++ c_o.py
@@ -1,2 +1,26 @@
 #classes and objects :
 
+class tour:
+    drink ='';
+    def party(self):#self keyword is must.
+        print('lets party....');
+    def beach(self):
+        print('Enjoy the beach');
+
+don = tour();
+
+don.party();
+don.beach();
+
+class person:
+    name = '';
+    age = '';
+    address = '';
+
+ramesh = person();
+
+ramesh.name = 'Ramesh';
+ramesh.age = 23;
+ramesh.address = 'chennai';
+
+print(ramesh.name,ramesh.age,ramesh.address);

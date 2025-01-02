cd5b4070931ed59089907e3a405e430cda788774
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-02 16:04:28 +0530

    py

--- c_2.py
+++ c_2.py
@@ -1,20 +1,48 @@
 #constructor and self keyword : 
 
-class oi:
-    def __init__(self):
-        self.name = '';  
-        self.age = 0;
-        self.gender = '';
+# class oi:
+#     def __init__(self):
+#         self.name = '';  
+#         self.age = 0;
+#         self.gender = '';
     
+#     def display(self):
+#         print(f'Name = {self.name}');
+#         print(f'Age = {self.age}');
+#         print(f'Gender = {self.gender}');
+
+# hi = oi();
+
+# hi.name = 'dk';
+# hi.age = 23;
+# hi.gender = 'male';
+
+# hi.display();
+
+
+# class student:
+#     def __init__(self):
+#         self.Name = '';
+#         self.Register_no = '';
+#     def display(self):
+#         print(f'Name of student = {self.Name}.');
+#         print(f'Register.no of student = {self.Register_no}.');
+
+
+# std_1 = student();
+# std_1.Name = 'Dk';
+# std_1.Register_no = '32056';
+
+# std_1.display();
+
+
+class Fruit():
+    def __init__(self):
+        self.color = '';
     def display(self):
-        print(f'Name = {self.name}');
-        print(f'Age = {self.age}');
-        print(f'Gender = {self.gender}');
+        print(self.color);
 
-hi = oi();
+apple = Fruit('red');
 
-hi.name = 'dk';
-hi.age = 23;
-hi.gender = 'male';
 
-hi.display();
+apple.display();

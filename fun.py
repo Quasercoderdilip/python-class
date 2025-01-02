6c5139f8c19e61f534be8d38245f4e280955fb42
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-02 11:55:17 +0530

    py

--- fun.py
+++ fun.py
@@ -7,18 +7,33 @@
 # callPerson('Mani');
 
 
-def add(a,b):
-    return a + b; 
-def sub(a,b):
-    return a - b;
-def mul(a,b):
-    return a * b;
-def div(a,b):
-    return a / b;
+# def add(a,b):
+#     return a + b; 
+# def sub(a,b):
+#     return a - b;
+# def mul(a,b):
+#     return a * b;
+# def div(a,b):
+#     return a / b;
 
 
 
-print(add(1,2));
-print(sub(1,2));
-print(mul(1,2));
-print(div(1,2));
+# print(add(1,2));
+# print(sub(1,2));
+# print(mul(1,2));
+# print(div(1,2));
+
+
+num_in = int(input('Enter the input : '));
+
+def check(num):
+    if num > 0 :
+        if num % 2 == 0:
+            print('even');
+        else:
+            print('odd');
+    else:
+        print('zero or below zero is cant be checked');
+
+
+check(num_in);

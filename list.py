27e60c68ff5512115794a4dec1adeabc41702bc9
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-02 10:49:30 +0530

    py

--- list.py
+++ list.py
@@ -7,17 +7,17 @@ from collections import OrderedDict
 # for fruit in fruits:
 #     print(fruit);
 
-setfruits = {'apple','orange','banana','pineapple','guova'};
+# setfruits = {'apple','orange','banana','pineapple','guova'};
 
-print(type(setfruits));
+# print(type(setfruits));
 
-my_list = list(OrderedDict.fromkeys(setfruits));
-print(my_list);
+# my_list = list(OrderedDict.fromkeys(setfruits));
+# print(my_list);
 
-my_list[3] = 'hoiii hoiii';
-print(my_list);
+# my_list[3] = 'hoiii hoiii';
+# print(my_list);
 
-setfruits = OrderedDict.fromkeys(my_list);
-#print(setfruits.keys());
-print(setfruits);
+# setfruits = OrderedDict.fromkeys(my_list);
+# #print(setfruits.keys());
+# print(setfruits);
 

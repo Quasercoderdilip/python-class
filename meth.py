b2b6b98099815c07a7222af9cf6578c7fc291aa9
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-02 20:34:32 +0530

    py

--- meth.py
+++ meth.py
@@ -1,4 +1,4 @@
-#class methods : 
+#class method : 
 
 #Class methods are methods that are bound to the class rather than an 
 #  of the class. They can be used to access and modify class-level data 
@@ -7,3 +7,32 @@
 # conventionally named cls.
 
 
+class pc:
+    ram = 32;
+    def __init__(self,cpu,proc,color):
+        self.cpu = cpu;
+        self.proc = proc;
+        self.color = color;
+    
+    def display(self):
+        print(f'CPU is {self.cpu}.');
+        print(f'processor is {self.proc}.');
+        print(f'color is {self.color}.');
+        print(f'Ram is {self.ram}GB')
+    
+    @classmethod           #class method
+    def changeRam(cls,new):
+        cls.ram = new;
+    
+    @staticmethod
+    def stat():
+        print('this is static method');
+
+
+soft = pc('i5','intel', 'black');
+soft.display();
+print();
+soft.changeRam(90);
+print();
+soft.display();
+soft.stat();

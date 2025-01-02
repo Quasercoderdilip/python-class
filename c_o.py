8d922180f45a2f7065e3ca2e60fd36d992aae8a3
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-02 15:12:57 +0530

    py

--- c_o.py
+++ c_o.py
@@ -25,32 +25,32 @@
 
 # print(ramesh.name,ramesh.age,ramesh.address);
 
-class laptop:
-    Brand = '';
-    price = '';
-    proccessor = '';
-    Ram = '';
+# class laptop:
+#     Brand = '';
+#     price = '';
+#     proccessor = '';
+#     Ram = '';
 
-lap_1 = laptop();
-lap_2 = laptop();
-lap_3 = laptop();
+# lap_1 = laptop();
+# lap_2 = laptop();
+# lap_3 = laptop();
 
-lap_1.Brand = str(input('Enter your laptop brand for laptop_1: '));
-lap_1.price = str(input('Enter your laptop price for laptop_1: '));
-lap_1.proccessor = str(input('Enter your laptop proccessor for laptop_1: '));
-lap_1.Ram = str(input('Enter your laptop Ram for laptop_1 : '));
-print();
-lap_2.Brand = str(input('Enter your laptop brand for laptop_2  : '));
-lap_2.price = str(input('Enter your laptop price for laptop_2 : '));
-lap_2.proccessor = str(input('Enter your laptop proccessor for laptop_2 : '));
-lap_2.Ram = str(input('Enter your laptop Ram for laptop_2 : '));
-print();
-lap_3.Brand = str(input('Enter your laptop brand for laptop_3 : '));
-lap_3.price = str(input('Enter your laptop price for laptop_3 : '));
-lap_3.proccessor = str(input('Enter your laptop proccessor for laptop_3 : '));
-lap_3.Ram = str(input('Enter your laptop Ram for laptop_3 : '));
-print();
+# lap_1.Brand = str(input('Enter your laptop brand for laptop_1: '));
+# lap_1.price = str(input('Enter your laptop price for laptop_1: '));
+# lap_1.proccessor = str(input('Enter your laptop proccessor for laptop_1: '));
+# lap_1.Ram = str(input('Enter your laptop Ram for laptop_1 : '));
+# print();
+# lap_2.Brand = str(input('Enter your laptop brand for laptop_2  : '));
+# lap_2.price = str(input('Enter your laptop price for laptop_2 : '));
+# lap_2.proccessor = str(input('Enter your laptop proccessor for laptop_2 : '));
+# lap_2.Ram = str(input('Enter your laptop Ram for laptop_2 : '));
+# print();
+# lap_3.Brand = str(input('Enter your laptop brand for laptop_3 : '));
+# lap_3.price = str(input('Enter your laptop price for laptop_3 : '));
+# lap_3.proccessor = str(input('Enter your laptop proccessor for laptop_3 : '));
+# lap_3.Ram = str(input('Enter your laptop Ram for laptop_3 : '));
+# print();
 
-print(lap_1.Brand,lap_1.price,lap_1.proccessor,lap_1.Ram);
-print(lap_2.Brand,lap_2.price,lap_2.proccessor,lap_2.Ram);
-print(lap_3.Brand,lap_3.price,lap_3.proccessor,lap_3.Ram);
+# print(lap_1.Brand,lap_1.price,lap_1.proccessor,lap_1.Ram);
+# print(lap_2.Brand,lap_2.price,lap_2.proccessor,lap_2.Ram);
+# print(lap_3.Brand,lap_3.price,lap_3.proccessor,lap_3.Ram);

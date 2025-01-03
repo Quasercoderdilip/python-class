c57df1fa74d96a162140d2a2197f7052b231e94f
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-04 04:48:10 +0530

    py

--- pyTasks/day_2.py
+++ pyTasks/day_2.py
@@ -1,7 +1,13 @@
 
 letters = ['a','b','c'];
 
-for i in letters:
-    for j in letters:
-        if i != j:
-            print(i,j);
+# for i in letters:
+#     for j in letters:
+#         if i != j:
+#             print(i+j);
+
+
+
+#another approach:
+combinations = [ i+j for i in letters for j in letters if i != j ];
+print(combinations);

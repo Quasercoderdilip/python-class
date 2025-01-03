51043032e1a374a284ad76f3ea0b1babeefdd10c
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-03 14:50:46 +0530

    py

--- poly.py
+++ poly.py
@@ -1,20 +1,23 @@
 #polymorphism : 
 
-class dad:
-    def action(self):#pubilc function.
-        print('He is a left hand user.');
+# class dad:
+#     def action(self, a = 0, b = 0, c = 0, d = 0):#pubilc function.
+#         print(f'result : {a+b+c+d}');
     
-    def __private(self):
-        print('this is a private function.');
+#     def __private(self):
+#         print('this is a private function.');
 
-class first_son(dad):
-    def Music(self):
-        print('singing beautifully');
-
-
+# class first_son(dad):
+#     def Music(self):
+#         print('singing beautifully');
 
 
-son_1 = first_son();
-son_1.action();
-son_1.Music();
+
+
+# son_1 = first_son();
+# son_1.action(2,1);#polymorphism
+# son_1.action(2,1,3);#polymorphism
+# son_1.action(2,1,3,4);#polymorphism
+# son_1.Music();
 # son_1._dad__private();#we can access private methods like this but it is not recommended.
+

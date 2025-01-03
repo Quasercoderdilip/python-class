0ae94fae355bbf70469d44b8333d11ca353ede84
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-03 14:32:07 +0530

    py

--- poly.py
+++ poly.py
@@ -1,2 +1,18 @@
 #polymorphism : 
 
+class dad:
+    def action(self):#pubilc function.
+        print('He is a left hand user.');
+    
+    def __private(self):
+        print('this is a private function.');
+
+class first_son(dad):
+    def Music(self):
+        print('singing beautifully');
+
+
+
+
+son_1 = first_son();
+son_1.__private();

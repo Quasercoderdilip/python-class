37151a302d9ca733029eef965aff8e101b1564e5
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-04 04:44:24 +0530

    py

--- pyTasks/day_1.py
+++ pyTasks/day_1.py
@@ -1,14 +1,9 @@
 
-num_starter = 1;
-num_limit = 100;
-except_nums = [];
+# for i in range(1,101):
+#     if i == 77 or i == 86 or i == 32 :
+#         continue;
+#     else:
+#         print(i);
 
-for i in range(num_starter,num_limit + 1):
-    if i == 77 or i == 86 or i == 32 :
-        except_nums.append(i)
-        continue;
-    else:
-        print(i);
-
-
-print(except_nums);
+#another approach:
+print(*(i for i in range(1, 101) if i not in {77, 86, 32}))
---
+++ pyTasks/day_2.py
@@ -0,0 +1,7 @@
+
+letters = ['a','b','c'];
+
+for i in letters:
+    for j in letters:
+        if i != j:
+            print(i,j);

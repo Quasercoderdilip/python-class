b37547c7de6ada6ecfa890be8007c93284f187ea
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-04 11:17:16 +0530

    py

--- pyqt5/sample.py
+++ pyqt5/sample.py
@@ -5,11 +5,14 @@ from PyQt5.QtWidgets import QApplication,QMainWindow
 class Main_window(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('Calculator');
+        self.setGeometry(700,300,500,600)
 
 def main():
     app = QApplication(sys.argv);
     window = Main_window();
     window.show();
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()

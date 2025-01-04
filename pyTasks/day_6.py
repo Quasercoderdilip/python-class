809684f8ea4a529fbf641bb16dfb0fe48db32b54
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-04 10:02:39 +0530

    py

--- pyTasks/day_6.py
+++ pyTasks/day_6.py
@@ -1,5 +1,5 @@
 distance_travelled = int(input('Enter the distance travelled : '));
-mileage_per_litre = 12;
+mileage_per_litre = float(input('Enter the mileage of your vehicle (in km/L): '))
 
 fuel = (distance_travelled / mileage_per_litre);
-print(f'Fuel : {fuel:.2f} litres')
+print(f'Fuel : {fuel:.2f} litres');

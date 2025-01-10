b9d9d1469f2e91cab5a2f0f60d29d58ea2ce1747
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-10 10:58:42 +0530

    py

--- pyTasks/day_7.py
+++ pyTasks/day_7.py
@@ -1,5 +1,23 @@
+class sorting_operations : 
+    def __init__(self):
+        print('you can sort the list here');
+
+    def manual_sorted_ans(self,get_val):#this function is using bubble sort algorithm.
+        self.len_arr = len(get_val)
+
+        for i in range(self.len_arr):
+            for j in range(0, self.len_arr - i - 1):
+                if get_val[j] > get_val[j + 1] :
+                    get_val[j], get_val[j+1] = get_val[j+1], get_val[j]
+
+        return get_val;
+
+    def sorted_ans(self,get_val):
+        self.sorted_list = sorted(get_val)#built-in sort method.
+        return self.sorted_list;
 
 storage = [];
+
 done = 'no'
 while done == 'no' :
     goal = str(input('enter your goal : '));
@@ -7,18 +25,10 @@ while done == 'no' :
     done = str(input('Are you done ? (yes/no)')).strip();
 
 
-def sorted_ans(get_val):
-
-    len_arr = len(get_val)
-
-    for i in range(len_arr):
-        for j in range(0, len_arr - i - 1):
-
-            if get_val[j] > get_val[i + 1] :
-
-                get_val[j], get_val[j+1] = get_val[j+1], get_val[j]
-
-    return get_val;
+store = sorting_operations();
+print(store.sorted_ans(storage));
 
 
-print(sorted_ans(storage));
+
+
+

19c9a0336cf54530740d2cf51c3d22f06cfcbcf6
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-15 19:10:14 +0530

    py

---
+++ prblms/09.py
@@ -0,0 +1,17 @@
+# *Question: Find the Sum of Digits of a Number*
+   
+
+def sum_num(num):
+    str_num = str(num);
+    sum = 0 ;
+    for i in str_num:
+        j = int(i);
+        sum += j
+
+    return sum
+    
+
+
+
+n = int(input('Enter the number : '));
+print(sum_num(n));

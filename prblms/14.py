92d4b850c6edb284442f7708e9d29d8fda4a371f
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-16 07:41:19 +0530

    py

--- prblms/14.py
+++ prblms/14.py
@@ -2,16 +2,13 @@
 
 
 def vowel_counter(para):
-    v = ['a','e','i','o','u'];
-    count = 0 ;
-    cl_para = para.replace(' ', '').replace('.', '').replace(',','');
-    for i in cl_para:
-        print(i)
-        for j in v:
-            if i == j :
-                count += 1;
+    v, count, cl_para = 'aeiou', 0, para.lower();
+    
+    for char in cl_para:
+        if char in v:
+            count += 1;
     return count;
-
+    
 
 p = 'Your code works correctly for calculating the power of a number. However, there are a few improvements that can make it more robust and easier to read. Let me first explain how your code';
 

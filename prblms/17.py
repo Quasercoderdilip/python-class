a083a3aa44d0d084ac173dc0d090dca67a66f997
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-16 20:26:29 +0530

    py

--- prblms/17.py
+++ prblms/17.py
@@ -1,1 +1,10 @@
+# *Question: Count Occurrences of a Character in a String*
 
+txt = input('Enter message : ')
+char = input('Enter character to check the count : ')
+
+if char in txt:
+    count = txt.count(char);
+    print(count)
+else:
+    print(f'Given character {char} is not in Message')

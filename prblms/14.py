872123c6e845c89859ef233e8e98814ccd56cbd9
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-16 07:09:46 +0530

    py

---
+++ prblms/14.py
@@ -0,0 +1,15 @@
+# *Question: Count Vowels in a String*
+
+
+para = 'Your code works correctly for calculating the power of a number. However, there are a few improvements that can make it more robust and easier to read. Let me first explain how your code';
+
+
+# v = ['a','e','i','o','u'];
+# count = 0 ;
+# for i in v:
+#     if i not in para:
+#         continue;
+#     else:
+#         count += 1;
+
+# print(count);

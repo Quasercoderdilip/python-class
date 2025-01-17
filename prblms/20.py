457060a69187a952e51aa4a1763c49d4673c5720
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-17 18:40:11 +0530

    py

--- prblms/20.py
+++ prblms/20.py
@@ -1,16 +1,19 @@
 # *Question: Check if Two Arrays Are Equal*
 
 
-def check_Equal(x,y):
+# def check_Equal(x,y):
 
-    if len(x) != len(y):
-        return False
+#     if len(x) != len(y):
+#         return False
    
-    for index in range(0,len(x)):
-        if x[index] != y[index]:
-            return False
+#     for index in range(0,len(x)):
+#         if x[index] != y[index]:
+#             return False
            
 
-    return True;
+#     return True;
 
-print(check_Equal(['Dilip', 'Sriram', 'Sairam'], ['Dilip', 'Sriram', 'Sairam']))
+def check_Equal(x, y):
+    return x == y
+
+print(check_Equal(['Dilip', 'Sriram', 'Sai'], ['Dilip', 'Sriram', 'Sairam']))

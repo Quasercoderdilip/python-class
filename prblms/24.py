73d205defaf38e59b69cbbc73e4a7af841dac307
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-18 02:23:43 +0530

    py

--- prblms/24.py
+++ prblms/24.py
@@ -2,10 +2,9 @@
 
 
 arr_1 = ['d','s','a','e','i']
-arr_2 = ['q','s','e','e','r']
+arr_2 = ['s','e','q','e','r']
 
 c_a = list(set(arr_1) & set(arr_2))
 
 print(c_a)
-
 
---
+++ prblms/25.py
@@ -0,0 +1,9 @@
+# *Question: Find the Difference Between Two Arrays*
+
+
+arr_1 = ['d','s','a','e','i']
+arr_2 = ['s','e','q','e','r']
+
+c_a = list(set(arr_1) ^ set(arr_2))
+
+print(c_a)
